9e60808989e75bf5a2ec04afe191430f324b46c7
Tal <trimoni8@gmail.com>
2022-12-05 10:00:20 -0500

    enter input

--- madlibs.py
+++ madlibs.py
@@ -1,6 +1,6 @@
-color = input('Enter a color')
-color = input('Enter a color')
-color = input('Enter a color')
+color = input('Enter a color: ')
+plural_noun = input('Enter a Plural Noun: ')
+celebrity = input('Enter a celebrity: ')
 
 
 print('Roses are {color}')
