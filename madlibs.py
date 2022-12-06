f8b9d29f6d061ae7d7d4ce278967c30313e1bb5b
Tal <trimoni8@gmail.com>
2022-12-06 14:56:21 -0500

    add more words

--- madlibs.py
+++ madlibs.py
@@ -1,6 +1,9 @@
 color = input('Enter a color: ')
 plural_noun = input('Enter a Plural Noun: ')
 celebrity = input('Enter a celebrity: ')
+noun = input('Enter a noun')
+adjective = input('Enter a adjective')
+animal = input('Enter a animal')
 
 
 print('Roses are ' + color)
