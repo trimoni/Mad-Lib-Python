13e539fbd37f19c4edfbb566191bdfddbb84937c
Tal <trimoni8@gmail.com>
2022-12-06 14:59:42 -0500

    finish mad lib

--- madlibs.py
+++ madlibs.py
@@ -1,14 +1,12 @@
 color = input('Enter a color: ')
 plural_noun = input('Enter a Plural Noun: ')
 celebrity = input('Enter a celebrity: ')
-noun = input('Enter a noun')
-adjective = input('Enter a adjective')
-animal = input('Enter a animal')
+noun = input('Enter a noun: ')
+adjective = input('Enter a adjective: ')
+animal = input('Enter a animal: ')
 
 
 print('Roses are ' + color)
 print(plural_noun + ' are blue')
 print('I love ' + celebrity)
 print('I keep feeling ' + adjective + ' because my ' + animal + ' is a ' + noun + ' !')
-print('I love ' + celebrity)
-print('I love ' + celebrity)
